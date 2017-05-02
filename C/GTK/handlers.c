59792d5ad05e727917a164b32e8ad0d701382428
Guilherme Ferreira <1150623@isep.ipp.pt>
2017-05-02 17:57:23 +0100

    better GUI than before, but still there is much to do :D

--- C/GTK/handlers.c
+++ C/GTK/handlers.c
@@ -1,53 +1,54 @@
 #include "header.h"
 
 
-/*Sets NextButton1 visible (in FolderPicker*/
-void set_Next_Visible(UNUSED, AppWidgets *app){
-
-	gtk_widget_show(app->nextButton1);
-	
-	char *folderPath;
-
-    folderPath = gtk_file_chooser_get_filename ((GtkFileChooser*) app->folderButton);
-    /*What to do with folder path?*/
-    printf("%s", folderPath);
-    
-    g_free (folderPath);
-    
-}
-
 /*Closes App*/
 void close_app(UNUSED, AppWidgets *app){
 	
 	//Code Before closing comes here...
-	
 	gtk_main_quit();
 	
 }
 
 /*Show FolderPicker Window + close Start Window*/
 void gtk_widget_showPicker(UNUSED, AppWidgets *app){
-	gtk_widget_hide(app->Start);
+	gtk_widget_destroy(app->Start);
 	gtk_widget_show(app->folderPicker);
-	gtk_widget_hide(app->nextButton1);
 }
 
-/*Default operations for FolderPicker window*/
-void do_picker_defaults(UNUSED, AppWidgets *app){
-	printf("\naaa");fflush(stdout);
-	printf("\nbbb");fflush(stdout);
-	
-	printf("\nccc");fflush(stdout);
+/*Show FolderPicker Window + close Start Window*/
+void gtk_widget_showPicker_again(UNUSED, AppWidgets *app){
+	gtk_widget_hide(app->selectNick);
+	gtk_widget_show(app->folderPicker);
 }
 
 /*Action when NEXT from FilePicker is clicked*/
 void folder_set(UNUSED, AppWidgets *app){
 	
-    
+	/*GET FOLDER:*/
+    gchar *folder =  gtk_file_chooser_get_uri ((GtkFileChooser*)app->folderButton);
+	if(folder != NULL){
+		printf("-------\nFOLDER: 	%s\n--------", folder);
+		
+		/**
+		 * WHAT TO DO WITH FOLDER?
+		 * 
+		 * */
+		
+		//avança...
+		gtk_widget_show(app->selectNick);
+		gtk_widget_hide(app->folderPicker);
+	}//else do nothing
+    g_free (folder);
 	
+}
+
+void finish(UNUSED, AppWidgets *app){
+	gtk_main_quit();
+	printf("\n---------\nApp closed\n---------\n");
 }
 
 /* Show the About box */
 void help_about(UNUSED, AppWidgets *app){
 	gtk_dialog_run(GTK_DIALOG(app->HelpAboutDialog));
+	gtk_widget_show(app->HelpAboutDialog);
 }
